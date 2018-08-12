2218baa162fa9cc3a1f1be050bb4c08d4e4e0bb0
Antonio Lok <antonio.lokchan@mail.utoronto.ca>
2018-08-12 02:35:32 -0400

    write review almost completed

--- src/components/WriteReview.js
+++ src/components/WriteReview.js
@@ -7,19 +7,44 @@ class WriteReview extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      text: null,
+      stars : null
     };
+  }
+
+  componentDidMount() {
+    sessionStorage.removeItem("signedUp");
+  }
+
+  handleChange(e) {
+    this.setState({[e.target.name] : e.target.value});
+  }
+
+  ratingChanged(value) {
+    this.setState({stars: value});
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    if (this.state.stars === null || this.state.text === null || this.state.text ==="") {
+      alert("Please fill out both the review and rating.")
+    } else {
+      
+    }
   }
 
   render() {
 
     const form = (
-      <form style={{textAlign:"center"}}>
+      <form style={{textAlign:"center"}} onSubmit={(event) => this.handleSubmit(event)}>
         <h5> Review for {(this.props.match.params.name)} </h5>
         <textarea id="review-input" 
           placeholder="Your review helps others learn about great local businesses. 
 
           Please don't review this business if you received a freebie for writing this review, or if you're connected in any way to the owner or employees."
-          maxlength="1000" 
+          maxLength="1000"
+          name="text"
+          onChange={(event) => this.handleChange(event)}
         ></textarea>
         <h1> Please give your rating: </h1>
         <ReactStars
@@ -27,6 +52,8 @@ class WriteReview extends Component {
           count={5}
           size={40}
           edit={true}
+          value={this.state.stars}
+          onChange={(newValue) => this.ratingChanged(newValue)}
           color2={'#ffd700'} />
       
       <button style={{width: "170px"}}> Post your review </button>
